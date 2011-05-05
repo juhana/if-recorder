cd18614cc15364f3c786b8be5c36d9ea49c30b27
Juhana Leinonen <git@nitku.net>
2011-05-05 14:59:12 +0000

    Transcript viewer warnings when there are gaps in the transcript

--- tools/viewer/transcript.php
+++ tools/viewer/transcript.php
@@ -5,8 +5,8 @@
 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="Robots" content="noindex,nofollow" />
-<link rel="stylesheet" type="text/css" href="../../../../../parchment/parchment.css">
-<link rel="stylesheet" type="text/css" href="transcript.css">
+<link rel="stylesheet" type="text/css" href="assets/parchment.css">
+<link rel="stylesheet" type="text/css" href="assets/transcript.css">
 
 </head>
 
@@ -16,8 +16,14 @@
 <?php 
 include_once( '../db.php' );
 
+// session id
 $session = $_GET[ 'session' ];
+
+// show warnings?
+$warnings = !( isset( $_GET[ 'warnings' ] ) && $_GET[ 'warnings' ] == '0' );
+
 $prevInputCount = 0;
+$prevOutputCount = 0;
 
 $query = $db->prepare( 'SELECT * FROM transcripts WHERE session = ? ORDER BY outputcount ASC' );
 $query->execute( array( $session ) ) or database_error( $query->errorInfo() );
@@ -25,6 +31,14 @@ $query->execute( array( $session ) ) or database_error( $query->errorInfo() );
 $rows = $query->fetchAll();
 
 foreach( $rows as $snippet ) {
+	// The output count should be continuous. If we've skipped a count, inform the user.
+	if( $prevOutputCount < $snippet[ 'outputcount' ] - 1 && $warnings ) {
+		echo '<div class="error">WARNING: Possible gap in the transcript</div>';
+	}
+	
+	$prevOutputCount = $snippet[ 'outputcount' ];
+
+	// When input count increments, we've started a new turn.
 	if( $prevInputCount != $snippet[ 'inputcount' ] ) {
 		echo '<span class="command">'.$snippet[ 'input' ].'</span><br />';
 		$prevInputCount = $snippet[ 'inputcount' ];
