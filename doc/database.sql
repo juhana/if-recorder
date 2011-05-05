31dcd07de134cb818cb6b0ffa792b394ea79dbfe
Juhana Leinonen <git@nitku.net>
2011-05-05 19:12:15 +0000

    Database structure

---
+++ doc/database.sql
@@ -0,0 +1,33 @@
+# MySQL database structure for Parchment transcript recorder plugin.
+#
+# http://code.google.com/p/parchment-transcript/
+
+
+CREATE TABLE `stories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session` varchar(255) NOT NULL,
+  `story` varchar(255) NOT NULL,
+  `started` datetime DEFAULT NULL,
+  `ended` datetime DEFAULT NULL,
+  `browser` varchar(255) DEFAULT NULL,
+  `interpreter` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 ;
+
+
+
+CREATE TABLE `transcripts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session` varchar(255) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `output` text NOT NULL,
+  `input` varchar(255) NOT NULL,
+  `turncount` int(11) DEFAULT NULL,
+  `inputcount` int(11) NOT NULL,
+  `outputcount` int(11) NOT NULL,
+  `window` int(11) NOT NULL,
+  `styles` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 ;
+
+
