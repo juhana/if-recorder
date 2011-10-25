0e22800772eac69572ca8b47597b843a0e280640
Juhana <Juhana@Kruununhaka-Windows>
2011-10-25 21:19:41 +0300

    System-independent path names

--- build/inform.py
+++ build/inform.py
@@ -1,16 +1,17 @@
+import os
 import glob
 import shutil
 
-destination = '../inform7/Recording Parchment/'
-files = glob.glob("../lib/*.js")
+destination = os.path.abspath( '../inform7/Recording Parchment' )
+files = glob.glob( os.path.abspath( "../lib/*.js" ) )
 
 # create the manifest file
-shutil.copy( '(manifest).txt.template', destination+'(manifest).txt' )
-manifest = open( destination+'(manifest).txt', 'a' )
+shutil.copy( '(manifest).txt.template', destination+'/(manifest).txt' )
+manifest = open( destination+'/(manifest).txt', 'a' )
 
 # copy library files over
-for file in files:
-    shutil.copy( file, destination )
-    manifest.write( file.rsplit( '\\', 1 )[ 1 ] +"\n" )
+for source in files:
+    shutil.copy( source, destination )
+    manifest.write( os.path.basename( source ) +"\n" )
 
 manifest.close()
