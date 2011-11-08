82c892a139da4e0ef851b738bf6789deb8806146
juhana <git@nitku.net>
2011-11-08 12:40:51 +0200

    Combined build scripts into one

--- build/minify.py
+++
@@ -1,9 +0,0 @@
-from subprocess import call
-
-call([ 
-      "/usr/bin/java", 
-      "-jar", "../../closure/closure-compiler/compiler.jar", 
-      "--js", "../src/if-recorder.js", 
-      "--compilation_level=SIMPLE_OPTIMIZATIONS", 
-      "--js_output_file", "../lib/if-recorder.min.js"
-])
